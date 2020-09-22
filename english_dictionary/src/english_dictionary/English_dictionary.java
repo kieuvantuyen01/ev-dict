249388f0fbe9f0f9f8b71dc102d7125202103c3f
daotung8801 <56082813+daotung8801@users.noreply.github.com>
2020-09-22 15:12:34 +0700

    Add files via upload

---
+++ english_dictionary/src/english_dictionary/English_dictionary.java
@@ -0,0 +1,19 @@
+package english_dictionary;
+
+public class English_dictionary {
+    
+    /**
+     * Ham test ket qua.
+     * @param args tra ve ket qua.
+     */
+    
+    public static void main(String[] args) {
+        DictionaryCommandline dictionary_cmd = new DictionaryCommandline();    
+        dictionary_cmd.dictionaryAdvanced();
+        dictionary_cmd.dictionarySearcher();
+        dictionary_cmd.addNewWord();
+        dictionary_cmd.deleteWord(); 
+        dictionary_cmd.showAllWords();
+        dictionary_cmd.dictionaryExportToFile();
+    } 
+}
